8911f306c25de5d75120dcc1441c443e8aa485fa
JonY <jonathanwyun@gmail.com>
2025-04-10 14:29:40 -0700

    Question 7 added
    additional print research

--- MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -3,7 +3,7 @@ package com.pluralsight;
 
 public class MathApp {
     public static void main(String[] args) {
-        //Question 1:
+        //Question 1: Find greater salary (higher variable)
         float bobSalary = 100.00f;
         float garySalary = 199.99f;
         float highestSalary;
@@ -12,7 +12,7 @@ public class MathApp {
 
         System.out.println("The highest salary is: " + highestSalary);
 
-        //Question 2:
+        //Question 2: Find lower price of 2 variables
         float carPrice = 10000f;
         float truckPrice = 20000f;
         float lowestVehiclePrice;
@@ -21,9 +21,24 @@ public class MathApp {
 
         System.out.println("The cheapest vehicle is: " + lowestVehiclePrice);
 
-        //Question 3:
+        //Question 3: Area of circle with radius 7.25
         double radius = 7.25;
         double area = Math.PI * radius * radius;
-        System.out.println("Area of cicle is" + area);
+        System.out.println("Area of circle is " + area);
+
+        //Question 7: Find and display random number from 0 - 1
+        double random = Math.random();
+        System.out.println(random);
+
+        System.out.print("Hello ");
+        System.out.print("World");
+        System.out.println();
+        System.out.println("Hello World");
+        System.out.println();
+
+        int id = 10135;
+        String name = "Brandon Plyers";
+        float pay = 5239.77123f;
+        System.out.printf("%s (id: %d) $%.2f", name, id, pay);
     }
 }
